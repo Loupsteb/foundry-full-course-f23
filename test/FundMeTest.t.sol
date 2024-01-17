d7c6e9389f1509d0836f9b7a271ecde7ce48e9d9
Loup <esteban.loup@gmail.com>
2024-01-17 13:30:46 +0100

    add testOnlyOwnerCanWithdraw test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -52,4 +52,14 @@ contract FundMeTest is Test {
         address funder = fundMe.getFunder(0);
         assertEq(funder, USER);
     }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+
+    }
 }
